eacb458e3c065a01b15cb6b31cc5c4b2e7b5e4a4
atomsfat <atomsmail@gmail.com>
2016-12-15 23:44:34 -0600

    Stick to r76 three.js again

--- workspace.js
+++ workspace.js
@@ -1,5 +1,5 @@
 /* global cpdefine chilipeppr cprequire */
-cprequire_test(["inline:com-chilipeppr-workspace-grbl-debug1"], function(ws) {
+cprequire_test(["inline:com-chilipeppr-workspace-grbl-debug2"], function(ws) {
 
     console.log("initting workspace");
 
@@ -33,12 +33,12 @@ cprequire_test(["inline:com-chilipeppr-workspace-grbl-debug1"], function(ws) {
 } /*end_test*/ );
 
 // This is the main definition of your widget. Give it a unique name.
-cpdefine("inline:com-chilipeppr-workspace-grbl-debug1", ["chilipeppr_ready"], function() {
+cpdefine("inline:com-chilipeppr-workspace-grbl-debug2", ["chilipeppr_ready"], function() {
     return {
         /**
          * The ID of the widget. You must define this and make it unique.
          */
-        id: "com-chilipeppr-workspace-grbl-debug1", // Make the id the same as the cpdefine id
+        id: "com-chilipeppr-workspace-grbl-debug2", // Make the id the same as the cpdefine id
         name: "Workspace / grbl", // The descriptive name of your widget.
         desc: `A ChiliPeppr Workspace grbl.`,
         url: "(auto fill by runme.js)", // The final URL of the working widget as a single HTML file with CSS and Javascript inlined. You can let runme.js auto fill this if you are using Cloud9.
@@ -834,7 +834,8 @@ cpdefine("inline:com-chilipeppr-workspace-grbl-debug1", ["chilipeppr_ready"], fu
             // http://jsfiddle.net/chilipeppr/y3HRF
             chilipeppr.load("#com-chilipeppr-3dviewer",
                 //"http://fiddle.jshell.net/chilipeppr/y3HRF/show/light/",
-                "http://raw.githubusercontent.com/chilipeppr/widget-3dviewer/master/auto-generated-widget.html",
+                // Three r76
+                    "https://raw.githubusercontent.com/chilipeppr/widget-3dviewer/375587f75fa1f6e73edc2092492860e159eaf4a2/auto-generated-widget.html",
 
                 function() {
                     console.log("got callback done loading 3d");
